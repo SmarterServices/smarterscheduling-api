59de09c2c4220958c8307dde463475d5005bc091
Muntasir Ahmed <muntasi.ahmed@enosisbd.com>
2018-09-16 19:15:51 +0600

    #160535876 - Generate Model for calender seat

---
+++ config/routes/schema/calender-seat.js
@@ -0,0 +1,225 @@
+
+'use strict';
+
+const joi = require('joi');
+
+let schema = {
+  add: {
+    params: joi
+      .object({
+        accountSid: joi
+          .string()
+          .required()
+          .description('Account Sid'),
+
+        locationSid: joi
+          .string()
+          .required()
+          .description('Location Sid'),
+
+        calendarSid: joi
+          .string()
+          .required()
+          .description('Calendar Sid')}),
+    payload:joi.object({
+     id: joi
+      .string()
+      .required()
+      .description('Id'),
+   isDeleted: joi
+      .boolean()
+      .required()
+      .description('Is Deleted'),
+   name: joi
+      .string()
+      .required()
+      .description('Name'),
+   createdDate: joi
+      .date()
+      .required()
+      .description('Created Date'),
+   lastModifiedDate: joi
+      .date()
+      .required()
+      .description('Last Modified Date'),
+   systemModstamp: joi
+      .date()
+      .required()
+      .description('System Modstamp'),
+   calendar: joi
+      .string()
+      .required()
+      .description('Calendar'),
+   calendarRSid: joi
+      .any()
+      .required()
+      .description('Calendar R Sid'),
+   seat: joi
+      .string()
+      .required()
+      .description('Seat'),
+   seatRSid: joi
+      .any()
+      .required()
+      .description('Seat R Sid')
+  })
+  .required()
+  .description('Calender Seat payload')
+  },
+  get: {
+    params: joi
+      .object({
+        accountSid: joi
+          .string()
+          .required()
+          .description('Account Sid'),
+
+        locationSid: joi
+          .string()
+          .required()
+          .description('Location Sid'),
+
+        calendarSid: joi
+          .string()
+          .required()
+          .description('Calendar Sid'),
+
+        calenderSeatSid: joi
+          .string()
+          .required()
+          .description('Calender Seat Sid')})
+  },
+  update: {
+    params: joi
+      .object({
+        accountSid: joi
+          .string()
+          .required()
+          .description('Account Sid'),
+
+        locationSid: joi
+          .string()
+          .required()
+          .description('Location Sid'),
+
+        calendarSid: joi
+          .string()
+          .required()
+          .description('Calendar Sid'),
+
+        calenderSeatSid: joi
+          .string()
+          .required()
+          .description('Calender Seat Sid')}),
+    payload: joi.object({
+     id: joi
+      .string()
+      .required()
+      .description('Id'),
+   isDeleted: joi
+      .boolean()
+      .required()
+      .description('Is Deleted'),
+   name: joi
+      .string()
+      .required()
+      .description('Name'),
+   createdDate: joi
+      .date()
+      .required()
+      .description('Created Date'),
+   lastModifiedDate: joi
+      .date()
+      .required()
+      .description('Last Modified Date'),
+   systemModstamp: joi
+      .date()
+      .required()
+      .description('System Modstamp'),
+   calendar: joi
+      .string()
+      .required()
+      .description('Calendar'),
+   calendarRSid: joi
+      .any()
+      .required()
+      .description('Calendar R Sid'),
+   seat: joi
+      .string()
+      .required()
+      .description('Seat'),
+   seatRSid: joi
+      .any()
+      .required()
+      .description('Seat R Sid')
+  })
+  .required()
+  .description('Calender Seat payload')
+  },
+  delete: {
+    params: joi
+      .object({
+        accountSid: joi
+          .string()
+          .required()
+          .description('Account Sid'),
+
+        locationSid: joi
+          .string()
+          .required()
+          .description('Location Sid'),
+
+        calendarSid: joi
+          .string()
+          .required()
+          .description('Calendar Sid'),
+
+        calenderSeatSid: joi
+          .string()
+          .required()
+          .description('Calender Seat Sid')})
+  },
+  list: {
+    params: joi
+      .object({
+        accountSid: joi
+          .string()
+          .required()
+          .description('Account Sid'),
+
+        locationSid: joi
+          .string()
+          .required()
+          .description('Location Sid'),
+
+        calendarSid: joi
+          .string()
+          .required()
+          .description('Calendar Sid')}),
+    query: {
+      offset: joi
+        .number()
+        .integer()
+        .min(0)
+        .description('Offset for the list'),
+      limit: joi
+        .number()
+        .integer()
+        .min(0)
+        .description('Number of items to return'),
+      sortKeys: joi
+        .array()
+        .items(joi.string())
+        .single()
+        .default([])
+        .description('Keys to sort the data'),
+      sortOrder: joi
+        .string()
+        .valid('ASC', 'DESC')
+        .description('Sort order')
+    }
+  }
+};
+
+module.exports = schema;
+    
