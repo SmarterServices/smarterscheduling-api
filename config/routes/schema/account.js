3ce102d9b017bee3ce6801effd9076269a69472b
Muntasir Ahmed <muntasi.ahmed@enosisbd.com>
2018-09-16 18:56:30 +0600

    #160535865 - Generate model for account

--- config/routes/schema/account.js
+++ config/routes/schema/account.js
@@ -1,24 +1,136 @@
+
 'use strict';
 
-const Joi = require('joi');
+const joi = require('joi');
 
-module.exports = Joi.object({
-  isDeleted: Joi
-    .boolean()
-    .required()
-    .description('Is Deleted'),
-  name: Joi
-    .string()
-    .required()
-    .description('Name'),
-  createdDate: Joi
-    .date()
-    .required()
-    .description('Created Date'),
-  systemModstamp: Joi
-    .date()
-    .required()
-    .description('System Modstamp')
-})
+let schema = {
+  add: {
+    params: joi
+      .object({}),
+    payload:joi.object({
+     id: joi
+      .string()
+      .required()
+      .description('Id'),
+   isDeleted: joi
+      .boolean()
+      .required()
+      .description('Is Deleted'),
+   name: joi
+      .string()
+      .required()
+      .description('Name'),
+   createdDate: joi
+      .date()
+      .required()
+      .description('Created Date'),
+   lastModifiedDate: joi
+      .date()
+      .required()
+      .description('Last Modified Date'),
+   systemModstamp: joi
+      .date()
+      .required()
+      .description('System Modstamp'),
+   title: joi
+      .string()
+      .required()
+      .description('Title'),
+   externalId: joi
+      .string()
+      .allow(null)
+      .description('External ID')
+  })
   .required()
-  .description('Account payload');
+  .description('Account payload')
+  },
+  get: {
+    params: joi
+      .object({
+        accountSid: joi
+          .string()
+          .required()
+          .description('Account Sid')})
+  },
+  update: {
+    params: joi
+      .object({
+        accountSid: joi
+          .string()
+          .required()
+          .description('Account Sid')}),
+    payload: joi.object({
+     id: joi
+      .string()
+      .required()
+      .description('Id'),
+   isDeleted: joi
+      .boolean()
+      .required()
+      .description('Is Deleted'),
+   name: joi
+      .string()
+      .required()
+      .description('Name'),
+   createdDate: joi
+      .date()
+      .required()
+      .description('Created Date'),
+   lastModifiedDate: joi
+      .date()
+      .required()
+      .description('Last Modified Date'),
+   systemModstamp: joi
+      .date()
+      .required()
+      .description('System Modstamp'),
+   title: joi
+      .string()
+      .required()
+      .description('Title'),
+   externalId: joi
+      .string()
+      .allow(null)
+      .description('External ID')
+  })
+  .required()
+  .description('Account payload')
+  },
+  delete: {
+    params: joi
+      .object({
+        accountSid: joi
+          .string()
+          .required()
+          .description('Account Sid')})
+  },
+  list: {
+    params: joi
+      .object({}),
+    query: {
+      offset: joi
+        .number()
+        .integer()
+        .min(0)
+        .description('Offset for the list'),
+      limit: joi
+        .number()
+        .integer()
+        .min(0)
+        .description('Number of items to return'),
+      sortKeys: joi
+        .array()
+        .items(joi.string())
+        .single()
+        .default([])
+        .description('Keys to sort the data'),
+      sortOrder: joi
+        .string()
+        .valid('ASC', 'DESC')
+        .description('Sort order')
+    }
+  }
+};
+
+module.exports = schema;
+    
