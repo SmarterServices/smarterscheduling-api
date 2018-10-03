a59a89903594153c28bb5eff4e921eb6fdb4eb1a
Muntasir Ahmed <muntasir.ahmed@enosisbd.com>
2018-10-03 17:32:21 +0600

    #160946716 - Add basic structure and joi for post appointment

--- config/routes/schema/appointment.js
+++ config/routes/schema/appointment.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const joi = require('joi');
+const utils = require('./../../../lib/helpers/utils');
 
 const schema = {
   add: {
@@ -8,94 +9,69 @@ const schema = {
       .object({
         accountSid: joi
           .string()
+          .regex(/^(SA)|(PA)[a-f0-9]{32}$/, 'Account Sid')
           .required()
           .description('Account Sid')
       }),
     payload: joi.object({
-      id: joi
+      seatSid: joi
         .string()
-        .required()
-        .description('Id'),
-      isDeleted: joi
-        .boolean()
-        .required()
-        .description('Is Deleted'),
-      name: joi
+        .regex(/^SE[a-f0-9]{32}$/, 'SeatSid')
+        .optional()
+        .description('Seat Sid'),
+      calendarSid: joi
         .string()
+        .regex(/^CL[a-f0-9]{32}$/, 'Calendar Sid')
         .required()
-        .description('Name'),
-      createdDate: joi
-        .date()
-        .required()
-        .description('Created Date'),
-      lastModifiedDate: joi
-        .date()
-        .required()
-        .description('Last Modified Date'),
-      systemModstamp: joi
-        .date()
-        .required()
-        .description('System Modstamp'),
-      seat: joi
+        .description('Calendar Sid'),
+      startDateTime: joi
         .string()
-        .required()
-        .description('Seat'),
-      seatRSid: joi
-        .any()
-        .required()
-        .description('Seat R Sid'),
-      calendar: joi
-        .string()
-        .required()
-        .description('Calendar'),
-      calendarRSid: joi
-        .any()
-        .required()
-        .description('Calendar R Sid'),
-      startDate: joi
-        .date()
+        .isoDate()
+        .regex(/.*Z/, 'ISO time format in utc zone')
+        .example(utils.dateTemplate(), 'date template')
         .required()
         .description('Start Date'),
-      endDate: joi
-        .date()
-        .required()
-        .description('End Date'),
+      duration: joi
+        .number()
+        .integer()
+        .positive()
+        .description('Duration of Exam'),
       externalId: joi
         .string()
-        .allow(null)
+        .allow(null, '')
+        .max(255)
         .description('External ID'),
       externalSystem: joi
         .string()
-        .allow(null)
+        .allow(null, '')
+        .max(255)
         .description('External System'),
       firstName: joi
         .string()
         .required()
-        .description('First Name'),
+        .description('The first name of the person making the reservation'),
       lastName: joi
         .string()
         .required()
-        .description('Last Name'),
+        .description('The last name of the person making the reservation'),
       email: joi
         .string()
         .required()
         .description('Email'),
       phone: joi
         .string()
-        .allow(null)
+        .allow(null, '')
         .description('Phone'),
       notes: joi
         .string()
-        .allow(null)
+        .allow(null, '')
         .description('Notes'),
       metadata: joi
-        .string()
-        .allow(null)
-        .description('Metadata'),
-      internalNotes: joi
-        .string()
-        .allow(null)
-        .description('Internal Notes')
+        .object()
+        .options({allowUnknown: true, stripUnknown: false})
+        .raw()
+        .default(null)
+        .description('Metadata')
     })
       .required()
       .description('Appointment payload')
@@ -179,10 +155,12 @@ const schema = {
       externalId: joi
         .string()
         .allow(null)
+        .default(null)
         .description('External ID'),
       externalSystem: joi
         .string()
         .allow(null)
+        .default(null)
         .description('External System'),
       firstName: joi
         .string()
@@ -199,10 +177,12 @@ const schema = {
       phone: joi
         .string()
         .allow(null)
+        .default(null)
         .description('Phone'),
       notes: joi
         .string()
         .allow(null)
+        .default(null)
         .description('Notes'),
       metadata: joi
         .string()
